63f83112913ad44a28883e03a34ceb051b195fa8
ad_itya07 <jhaaditya2207@gmail.com>
2025-01-19 20:31:35 +0530

    integrated projects from use-Project

--- src/app/(protected)/_components/AppSidebar.tsx
+++ src/app/(protected)/_components/AppSidebar.tsx
@@ -13,6 +13,7 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
+import useProject from "@/hooks/use-project";
 import { cn } from "@/lib/utils";
 import {
   Bot,
@@ -50,23 +51,11 @@ const items = [
   },
 ];
 
-const projects = [
-  {
-    name: "Project 1",
-  },
-  {
-    name: "Project 2",
-  },
-  {
-    name: "Project 3",
-  },
-  {
-    name: "Project 4",
-  },
-];
 
 const AppSidebar = ({}: Props) => {
   const pathname = usePathname();
+
+  const {projects, projectId, setProjectId} = useProject();
 
   const { open } = useSidebar();
   return (
@@ -109,16 +98,16 @@ const AppSidebar = ({}: Props) => {
           <SidebarGroupLabel>Your Projects</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
-              {projects.map((project) => {
+              {projects?.map((project) => {
                 return (
                   <SidebarMenuItem key={project.name}>
                     <SidebarMenuButton asChild>
-                      <div>
+                      <div onClick={() => setProjectId(project.id)}>
                         <div
                           className={cn(
                             "flex size-6 items-center justify-center rounded-sm border bg-white text-sm text-primary",
                             {
-                              "bg-primary text-white": true, // TODO: check if project is active, i.e check the project id
+                              "bg-primary text-white": project.id === projectId,
                             },
                           )}
                         >
