0b0fee852c443864d1e33eb63c95c376fbfa868e
ad_itya07 <jhaaditya2207@gmail.com>
2025-01-22 10:30:10 +0530

    ask-question-card comp.

---
+++ src/app/(protected)/dashboard/_components/AskQuestionCard.tsx
@@ -0,0 +1,90 @@
+"use client";
+import MDEditor from "@uiw/react-md-editor"
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Textarea } from "@/components/ui/textarea";
+import useProject from "@/hooks/use-project";
+import Image from "next/image";
+import { useState } from "react";
+import { askQuestion } from "../actions";
+import { readStreamableValue } from "ai/rsc";
+import CodeReferences from "./CodeReferences";
+
+type Props = {};
+
+const AskQuestionCard = ({}: Props) => {
+  const { project } = useProject();
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [question, setQuestion] = useState("");
+  const [fileReferences, setFileReferences] = useState<
+    { fileName: string; sourceCode: string; summary: string }[]
+  >([]);
+  const [answer, setAnswer] = useState("");
+
+  const onSubmit = async (e: React.FormEvent) => {
+    setAnswer("");
+    setFileReferences([]);
+    e.preventDefault();
+    if (!project?.id) return;
+    setLoading(true);
+
+    const { output, fileReferences } = await askQuestion(project.id, question);
+    setOpen(true);
+
+    for await (const delta of readStreamableValue(output)) {
+        if (delta) {
+            setAnswer(ans => ans + delta);
+        }
+    }
+
+    setLoading(false);
+  };
+  return (
+    <>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent className="sm:max-w-[80vw]">
+          <DialogHeader>
+            <DialogTitle>
+              <Image src="/logo.png" alt="dionysus" width={40} height={40} />
+            </DialogTitle>
+          </DialogHeader>
+
+          <MDEditor.Markdown source={answer} className="max-w-[70vw] !h-full max-h-[40vh] overflow-scroll" />
+
+          <div className="h-4"></div>
+          
+          <CodeReferences fileReferences={fileReferences} />
+
+            <Button type="button" onClick={() => setOpen(false)}>
+              Close
+            </Button>
+        </DialogContent>
+      </Dialog>
+      <Card className="relative col-span-3">
+        <CardHeader>
+          <CardTitle>Ask a question</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={onSubmit}>
+            <Textarea
+              placeholder="Which file should I edit to change the homepage?"
+              value={question}
+              onChange={(e) => setQuestion(e.target.value)}
+            />
+            <div className="h-4"></div>
+            <Button type="submit" disabled={loading}>Ask Dionysus!</Button>
+          </form>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
+
+export default AskQuestionCard;
