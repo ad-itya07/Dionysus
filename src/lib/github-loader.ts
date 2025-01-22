3f2a5aefa16a071a45a6d8126c38d34c46d6db72
ad_itya07 <jhaaditya2207@gmail.com>
2025-01-22 10:22:59 +0530

    added: index-github-repo & generate-embeddings funcs

--- src/lib/github-loader.ts
+++ src/lib/github-loader.ts
@@ -1,16 +1,74 @@
-import { GithubRepoLoader } from "@langchain/community/document_loaders/web/github"
+import { GithubRepoLoader } from "@langchain/community/document_loaders/web/github";
+import { Document } from "@langchain/core/documents";
+import { generateEmbedding, summariseCode } from "./gemini";
+import { db } from "@/server/db";
 
-export const loadGithubRepo = async (githubUrl: string, githubToken?: string) => {
-    const loader = new GithubRepoLoader(githubUrl, {
-        accessToken: githubToken || '',
-        branch: 'main',
-        ignoreFiles: ['package.json', 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml'],
-        recursive: true, // Load all files in the repo, by default only the root directory is loaded, not the files inside it
-        unknown: 'warn',
-        maxConcurrency: 5,
-    });
+export const loadGithubRepo = async (
+  githubUrl: string,
+  githubToken?: string,
+) => {
+  const loader = new GithubRepoLoader(githubUrl, {
+    accessToken: githubToken || "",
+    branch: "main",
+    ignoreFiles: [
+      "package.json",
+      "package-lock.json",
+      "yarn.lock",
+      "pnpm-lock.yaml",
+    ],
+    recursive: true, // Load all files in the repo, by default only the root directory is loaded, not the files inside it
+    unknown: "warn",
+    maxConcurrency: 5,
+  });
 
-    const docs = await loader.load();
-    return docs;
-}
+  const docs = await loader.load();
+  return docs;
+};
 
+export const indexGithubRepo = async (
+  projectId: string,
+  githubUrl: string,
+  githubToken?: string,
+) => {
+  const docs = await loadGithubRepo(githubUrl, githubToken);
+  const allEmbeddings = await generateEmbeddings(docs); // this func here does two things, gets the summary of the code, as well as the embeddings.
+
+  await Promise.allSettled(
+    allEmbeddings.map(async (embedding, index) => {
+      console.log(`processing ${index} of ${allEmbeddings.length - 1}`);
+
+      if (!embedding) return;
+
+      const sourceCodeEmbedding = await db.sourceCodeEmbedding.create({
+        data: {
+          summary: embedding.summary,
+          sourceCode: embedding.sourceCode,
+          fileName: embedding.fileName,
+          projectId: projectId,
+        },
+      });
+
+      await db.$executeRaw`
+      UPDATE "SourceCodeEmbedding"
+      SET "summaryEmbedding" = ${embedding.embedding}::vector
+      WHERE "id" = ${sourceCodeEmbedding.id}
+      `;
+    }),
+  );
+};
+
+const generateEmbeddings = async (docs: Document[]) => {
+  return await Promise.all(
+    docs.map(async (doc) => {
+      const summary = await summariseCode(doc);
+      const embedding = await generateEmbedding(summary);
+
+      return {
+        summary,
+        embedding,
+        sourceCode: JSON.parse(JSON.stringify(doc.pageContent)),
+        fileName: doc.metadata.source,
+      };
+    }),
+  );
+};
