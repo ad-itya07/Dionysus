302753ba7bb8be770c418d4410f9b7170e579b09
ad_itya07 <jhaaditya2207@gmail.com>
2025-01-22 10:27:27 +0530

    ask-question server action

---
+++ src/app/(protected)/dashboard/actions.ts
@@ -0,0 +1,82 @@
+"use server";
+import { Output, streamText } from "ai";
+import { createStreamableValue } from "ai/rsc";
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { generateEmbedding } from "@/lib/gemini";
+import { db } from "@/server/db";
+
+const google = createGoogleGenerativeAI({
+  apiKey: process.env.GEMINI_API_KEY,
+});
+
+export const askQuestion = async (question: string, projectId: string) => {
+  const stream = createStreamableValue();
+
+  //   HERE WE CONVERT THE QUESTION INTO A VECTOR
+  const queryVector = await generateEmbedding(question);
+  const vectorQuery = `[${queryVector.join(",")}]`;
+
+  //   WE QUERY THE DATABASE FOR SIMILARITY, AND FETCH TOP 10 RESULTS
+  const result = await db.$queryRaw`
+    SELECT "fileName", "sourceCode", "summary",
+    1 - ("summaryEmbedding" <=> ${vectorQuery}::vector) AS similarity
+    FROM "SourceCodeEmbedding"
+    WHERE 1 - ("summaryEmbedding" <=> ${vectorQuery}::vector) > .1
+    AND "projectId" = ${projectId}
+    ORDER BY similarity DESC
+    LIMIT 10
+    ` as { fileName: string; sourceCode: string; summary: string }[];
+
+  console.log("this is the result:", result);
+  //  WE CREATE A CONTEXT BLOCK TO PROVIDE CONTEXT TO THE AI
+  let context = "";
+  for (const doc of result) {
+    context += `source: ${doc.fileName}\ncode content: ${doc.sourceCode}\nsummary of file: ${doc.summary}\n\n`;
+  }
+
+  //   WE STREAM THE QUESTION AND CONTEXT TO THE AI
+  (async () => {
+    const { textStream } = await streamText({
+      model: google("gemini-1.5-flash"),
+      prompt: `
+        You are a AI code assistant who answers questions about the codebase. Your target audience is a technical intern who is looking to understand the codebase.
+
+AI assistant is a brand new, powerful, human-like artificial intelligence. The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
+
+AI is a well-behaved and well-mannered individual.
+AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.
+
+AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.
+
+If the question is asking about code or a specific file, AI will provide the detailed answer, giving step by step instructions, including code snippets.
+
+START CONTEXT BLOCK
+${context}
+END CONTEXT BLOCK
+
+START QUESTION
+${question}
+END QUESTION
+
+AI assistant will take into account any CONTEXT BLOCK that is provided in conversation.
+If the context doesnot provide the answer to question, the AI assistant will say, 'I am sorry, I do not have the answer to that question'.
+AI assistant will not apologize for the previous responses, but instead will indicate new information was gained.
+AI assistant will not invent anything that is not drawn directly from the context.
+Answer in markdown syntax, with code snippets if needed. Be as detailed as possible when answering, make sure to include all relevant information.
+        `,
+    });
+
+    // CREATE STREAM USING THE TEXT-STREAM FROM AI RESPONSE
+    for await (const delta of textStream) {
+      stream.update(delta);
+    }
+
+    stream.done();
+  })();
+
+  //   RETURN THE OUTPUT AND FILE REFERENCES
+  return {
+    output: stream.value,
+    fileReferences: result,
+  };
+};
