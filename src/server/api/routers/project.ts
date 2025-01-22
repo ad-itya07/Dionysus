05e22f2ce3118f8a2d1a430f9e94540741ec2bed
ad_itya07 <jhaaditya2207@gmail.com>
2025-01-22 23:58:56 +0530

    added: save-answer & get-questions action

--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
-import { pollCommits } from "@/lib/github";
+import { pullCommits } from "@/lib/github";
 import { indexGithubRepo } from "@/lib/github-loader";
 
 export const projectRouter = createTRPCRouter({
@@ -9,14 +9,24 @@ export const projectRouter = createTRPCRouter({
       z.object({
         name: z.string(),
         githubUrl: z.string(),
-        githubToken: z.string(),
+        githubToken: z.string().optional(),
       }),
     )
     .mutation(async ({ ctx, input }) => {
+      const user = await ctx.db.user.findUnique({
+        where: { id: ctx.user.userId! },
+        select: { credits: true },
+      });
+      if (!user) {
+        throw new Error("User not found");
+      }
+
+      const currentCredits = user.credits || 0;
+
       const project = await ctx.db.project.create({
         data: {
-          name: input.name,
           githubUrl: input.githubUrl,
+          name: input.name,
           userToProjects: {
             create: {
               userId: ctx.user.userId!,
@@ -25,7 +35,7 @@ export const projectRouter = createTRPCRouter({
         },
       });
       await indexGithubRepo(project.id, input.githubUrl, input.githubToken);
-      await pollCommits(project.id);
+      await pullCommits(project.id);
       return project;
     }),
 
@@ -41,7 +51,6 @@ export const projectRouter = createTRPCRouter({
       },
     });
   }),
-
   getCommits: protectedProcedure
     .input(
       z.object({
@@ -49,10 +58,47 @@ export const projectRouter = createTRPCRouter({
       }),
     )
     .query(async ({ ctx, input }) => {
-      pollCommits(input.projectId).then().catch(console.error);
+      pullCommits(input.projectId).then().catch(console.error);
       return await ctx.db.commit.findMany({
+        where: { projectId: input.projectId },
+      });
+    }),
+  saveAnswer: protectedProcedure
+    .input(
+      z.object({
+        projectId: z.string(),
+        question: z.string(),
+        answer: z.string(),
+        filesReferences: z.any(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.db.question.create({
+        data: {
+          answer: input.answer,
+          filesReferences: input.filesReferences,
+          projectId: input.projectId,
+          question: input.question,
+          userId: ctx.user.userId!,
+        },
+      });
+    }),
+  getQuestions: protectedProcedure
+    .input(
+      z.object({
+        projectId: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      return await ctx.db.question.findMany({
         where: {
           projectId: input.projectId,
+        },
+        include: {
+          user: true,
+        },
+        orderBy: {
+          createdAt: "desc",
         },
       });
     }),
