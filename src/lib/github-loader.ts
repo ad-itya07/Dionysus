0e60e083be51d84c5448b7a79f048dfe5fbf3b4d
ad_itya07 <jhaaditya2207@gmail.com>
2025-01-23 20:11:16 +0530

    added: get-file-count & check-credits func.

--- src/lib/github-loader.ts
+++ src/lib/github-loader.ts
@@ -2,6 +2,7 @@ import { GithubRepoLoader } from "@langchain/community/document_loaders/web/gith
 import { Document } from "@langchain/core/documents";
 import { generateEmbedding, summariseCode } from "./gemini";
 import { db } from "@/server/db";
+import { Octokit } from "octokit";
 
 export const loadGithubRepo = async (
   githubUrl: string,
@@ -71,4 +72,69 @@ const generateEmbeddings = async (docs: Document[]) => {
       };
     }),
   );
+};
+
+//recursive function to calculate number of files in a github repository.
+const getFileCount = async (
+  path: string,
+  octokit: Octokit,
+  githubOwner: string,
+  githubRepo: string,
+  acc: number = 0,
+) => {
+  // get the contents of the path
+  const { data } = await octokit.rest.repos.getContent({
+    owner: githubOwner,
+    repo: githubRepo,
+    path,
+  });
+
+  // if data is not array, then it is just one file
+  if (!Array.isArray(data) && data.type === "file") {
+    return acc + 1;
+  }
+
+  // if data is array, means there are subfolders
+  if (Array.isArray(data)) {
+    let fileCount = 0;
+    const directories: string[] = [];
+
+    for (const item of data) {
+      // if item is a directory, add it to the list of directories. else, fileCount++
+      if (item.type === "dir") {
+        directories.push(item.path);
+      } else {
+        fileCount++;
+      }
+    }
+
+    // Now if number of directories is more than 0, then do a recursive call to  getFileCount to each directory
+    if (directories.length > 0) {
+      const directoryCounts = await Promise.all(
+        directories.map((dirPath) =>
+          getFileCount(dirPath, octokit, githubOwner, githubRepo, 0),
+        ),
+      );
+
+      fileCount += directoryCounts.reduce((acc, count) => acc + count, 0);
+    }
+
+    return acc + fileCount;
+  }
+
+  return acc;
+};
+
+export const checkCredits = async (githubUrl: string, githubToken?: string) => {
+  // find out how many files are in the repo
+  const octokit = new Octokit({ auth: githubToken });
+
+  const githubOwner = githubUrl.split("/")[3];
+  const githubRepo = githubUrl.split("/")[4];
+  if (!githubOwner || !githubRepo) {
+    return 0;
+  }
+
+  const fileCount = await getFileCount("", octokit, githubOwner, githubRepo, 0);
+  return fileCount;
 };
